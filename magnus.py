7c133336622fda69df12a65071a17b2dc0978642
Furkan Çentek <56794915+rootcf@users.noreply.github.com>
2022-12-07 04:19:48 +0300

    Update magnus.py

--- magnus.py
+++ magnus.py
@@ -7,7 +7,6 @@ import sys
 sys.path.append("./commands")
 mic = sr.Microphone() 
 r = sr.Recognizer()
-
 komutlar = {}
 
 for dosya in os.listdir("commands"):
@@ -19,15 +18,18 @@ for dosya in os.listdir("commands"):
     komutlar.update(modul.exec)
 
 def callback(recognizer, audio):
-   r.adjust_for_ambient_noise(mic)
+    r.adjust_for_ambient_noise(mic)
     try:
-        yazi = r.recognize_google(audio, language='tr-tr')
+        yazi = r.recognize_google(audio, language="tr-tr")
         yazi = yazi.lower()
         command = " ".join(yazi.split()[1:]).lower()
         
-        if yazi.split()[0] == "magnus" or "magnoz": 
+        if yazi.split()[0] == "magnus" or "magnoz" or "magnos": 
           if command in komutlar:
-              komutlar[command]["exec"]()      
+              komutlar[command]["exec"](command.split()[1:])      
+          elif command.split()[0] in komutlar:
+              komutlar[command.split()[0]]["exec"](" ".join(command.split()[1:]))      
+
 
     except sr.WaitTimeoutError:
         print("Dinleme zaman aşımına uğradı")
@@ -37,6 +39,6 @@ def callback(recognizer, audio):
 
     except sr.RequestError:
         print("İnternete bağlanamıyorum")
-        
+
 r.listen_in_background(mic, callback)
-while True: time.sleep(0.0001)
+while True: time.sleep(0.1)
